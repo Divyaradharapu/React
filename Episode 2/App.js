f4dafdb39ffd5afafd58e7833ae6522ceb70bfa7
DivyaRadharapu <radharapudivya2001@gmail.com>
2026-01-11 15:28:01 +0530

    episode-2

--- Episode 2/App.js
+++ Episode 2/App.js
@@ -3,3 +3,7 @@ import ReactDOM from 'react-dom/client'
 const heading = React.createElement("h1",{},"HelloWorld");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+
+const Heading = () =>{
+    return <h1>Namaste React</h1>
+}; 
---
+++ Episode 2/dist/Episode 2.6bd02f5a.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=Episode 2.6bd02f5a.js.map
